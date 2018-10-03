f063fef99c7f2d62e7dd132a35d7d6ac5449e30c
@Justin_mm <justinmacharia78@gmail.com>
2018-10-03 16:39:11 +0300

    Simple arrays examples

---
+++ HelloWorld/simpleArrays/simpleArrays/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 23, 24, 25, 17, 28, 38, 17, 19 };
+            string[] names = new string[] {"James", "Kendi", "Lucy", "Robin"};
+
+            //print all the numbers in the numbers array
+            foreach (int number in numbers)
+            {
+                Console.Write("{0}, ", number);
+            }
+
+            //Reverse the second items in the names array
+            char[] charArray = names[1].ToCharArray();
+            Array.Reverse(charArray);
+            foreach (char character in charArray)
+            {
+                Console.Write(character);
+            }
+            Console.ReadLine();
+        }
+    }
+}
