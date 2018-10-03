383680fc78fa0dccf0f47574ec7046183b876416
@Justin_mm <justinmacharia78@gmail.com>
2018-10-03 12:33:11 +0300

    Data types and variables tutorial

---
+++ HelloWorld/dataTypesAndVariables/dataTypesAndVariables/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dataTypesAndVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int x = 5;
+            int y = x + 2;
+            Console.WriteLine(y);
+            */
+
+            Console.WriteLine("Hey, What's your first name?");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Now tell me your Last name:-)");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Hi " + firstName + " " + lastName + ", I love You!");
+            Console.ReadLine();
+
+        }
+    }
+}
