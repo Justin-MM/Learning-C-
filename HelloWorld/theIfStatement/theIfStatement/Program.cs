25165142fa8ebd4dca56ca6895867f739ff1684b
@Justin_mm <justinmacharia78@gmail.com>
2018-10-03 13:09:23 +0300

    The IF statement

---
+++ HelloWorld/theIfStatement/theIfStatement/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace theIfStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Awesome Giveaways today! just choose a number");
+            Console.WriteLine("Hi User, Will it be 1, 2 or 3?");
+            string userValue = Console.ReadLine();
+
+            if (userValue == "2")
+            {
+                Console.WriteLine("Yeessss! You won a Ford Ranger");
+            }
+            else if (userValue == "1" | userValue == "3")
+            {
+                Console.WriteLine("Sorry! You are out of luck today.");
+            }
+            else
+            {
+                Console.WriteLine("Sorry! That's invalid. Try again with a number between 1 and 3");
+            }
+            Console.ReadLine();
+        }
+    }
+}
