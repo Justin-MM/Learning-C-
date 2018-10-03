ecda9dd8eb3298c540ba839e385e1f8a4c3b176b
@Justin_mm <justinmacharia78@gmail.com>
2018-10-03 15:22:19 +0300

    For statements

---
+++ HelloWorld/forStatement/forStatement/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forStatement
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                string message = (i == 9) ? "The End" : "Just another number";
+                Console.WriteLine(message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
