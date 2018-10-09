7c3a91bea73a3d392de9cc7640c87a13b9a9e88b
@Justin_mm <justinmacharia78@gmail.com>
2018-10-09 12:54:11 +0300

    working with strings, while loops and do while loops

---
+++ HelloWorld/Strings/Strings/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string literals
+            int num1 = 2;
+            int num2 = 3;
+            string myString = String.Format("Wow! Today I have {0} C# assignments and {1} python assignments",
+                num1, num2);
+            Console.WriteLine(myString);
+            myString = String.Format("Wow! Today I have {1} C# assignments and {0} python assignments",
+                num1, num2);
+            Console.WriteLine(myString);
+            myString = String.Format("Wow! Today I have {1} C# assignments and {1} python assignments",
+                num1, num2);
+            Console.WriteLine(myString);
+
+            //string.format - currency
+            myString = String.Format("Hey, I need {0:C} to buy a classy shoe", 300.00);
+            Console.WriteLine(myString);
+
+            //string.format - Reading long Numbers
+            myString = String.Format("I heard that Trump is worth {0:N} dollars. Is it true?", 2000000000.00);
+            Console.WriteLine(myString);
+
+            //string.format - Phone Numbers
+            myString = String.Format("Hey,my old phone number is +{0:(###) ## #######}", 254735353158);
+            Console.WriteLine(myString);
+
+            //string.format - Percentage
+            string newString = string.Format("I scored a {0:P} in calculus", .98);
+            Console.WriteLine(newString);
+
+            //capitalize
+            Console.WriteLine(newString.ToUpper());
+
+            //find and replace
+            Console.WriteLine(newString.Replace("scored", "achieved"));
+
+            //escape characters
+            newString = string.Format("my cat nickname is \"meow\"");
+            Console.WriteLine(newString);
+
+            newString = @"   most of my programs are in my C:\ drive  ";
+            Console.WriteLine(newString);
+
+            //StringBuilder class for efficiency
+            StringBuilder anotherString = new StringBuilder();
+            anotherString.Append("I know Barack Obama! ");
+            anotherString.Append("Wow! this is cool");
+            Console.WriteLine(anotherString);
+
+            //trimming leading and ending spaces
+            newString.Trim();
+
+            //length of a string
+            int length = newString.Length;
+            Console.WriteLine(length);
+
+            //get a substring of a string
+            newString = newString.Substring(7);
+            Console.WriteLine(newString);
+
+            //convert an int to a string
+            Console.WriteLine("my number is {0}", num1.ToString());
+
+            //delete a part of a string
+            Console.WriteLine(newString.Remove(7,18));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HelloWorld/whileLoops/whileLoops/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whileLoops
+{
+    //display a main menu
+    //then 1) print the numbers a user wants
+    //2)play a guessing game
+    //3)exit out of the application
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool showMainMenu = true;
+
+            while (showMainMenu)
+            {
+                Console.Clear();
+                showMainMenu = MainMenu();
+            }
+        }
+
+        private static bool MainMenu()
+        {
+            Console.WriteLine("Choose an item number from the menu:");
+            Console.WriteLine("1) Print some numbers");
+            Console.WriteLine("2) Play a guessing game");
+            Console.WriteLine("3) Exit");
+            int userChoice = int.Parse(Console.ReadLine());
+
+            if (userChoice == 1)
+            {
+                Console.Clear();
+                PrintNumbers();
+                return true;
+            }
+            else if (userChoice == 2)
+            {
+                Console.Clear();
+                GuessingGame();
+                return true;
+            }
+            else if (userChoice == 3)
+            {
+                Console.WriteLine("Goodbye!");
+                return false;
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Please choose an item from the Menu");
+                Console.ReadLine();
+                return true;
+            }
+
+        }
+
+        private static void PrintNumbers()
+        {
+            Console.WriteLine("Print Numbers");
+            Console.Write("How many numbers do you want?: ");
+            int numbers = int.Parse(Console.ReadLine());
+
+            int counter = 0;
+            while (counter < numbers + 1)
+            {
+                Console.Write(counter);
+                Console.Write("-");
+                counter++;
+            }
+            Console.ReadLine();
+
+        }
+
+        private static void GuessingGame()
+        {
+            Console.WriteLine("Let's Play a guessing game!");
+            Random randomNumber = new Random();
+            int guessThisNumber = randomNumber.Next(1, 11);
+            int guesses = 0;
+                        
+            bool incorrect = true;
+            do
+            {
+                Console.WriteLine("Guess a number between 1 and 10: ");
+                int answer = int.Parse(Console.ReadLine());
+                guesses++;
+
+                if (answer == guessThisNumber)
+                {
+                    incorrect = false;
+                }
+                else {
+                    Console.WriteLine("Wrong!");
+                    incorrect = true;
+                }
+            } while (incorrect);
+            Console.WriteLine("Correct! It took you {0} guesses to get it right", guesses);
+            Console.ReadLine();
+
+        }
+    }
+}
