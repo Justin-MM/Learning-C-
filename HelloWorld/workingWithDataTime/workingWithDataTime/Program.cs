49aa9d4632fc6b50de9212a3315c1e57b2db85c6
@Justin_mm <justinmacharia78@gmail.com>
2018-10-09 13:28:07 +0300

    working with dates and time

---
+++ HelloWorld/workingWithDataTime/workingWithDataTime/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace workingWithDataTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //show today's date
+            DateTime date = DateTime.Now;
+            Console.WriteLine(date.ToString());
+
+            Console.WriteLine(date.ToLongDateString());
+            Console.WriteLine(date.ToLongTimeString());
+
+            //one week ago
+            Console.WriteLine(date.AddDays(-7).ToLongDateString());
+
+            //my birthday
+            DateTime Age = new DateTime(1993, 08, 30);
+            Console.WriteLine(Age.ToLongDateString());
+
+            //my age
+            Console.WriteLine(date.Subtract(Age).TotalDays / 365);
+
+            TimeSpan myAge = DateTime.Now.Subtract(Age);
+            Console.WriteLine(myAge.TotalDays.ToString());
+            Console.ReadLine();
+        }
+    }
+}
