bf678595fabe8b6f7cc0cc2bcb232dd6860e48bd
@Justin_mm <justinmacharia78@gmail.com>
2018-10-30 17:51:11 +0300

    access azure blob storage programmatically

---
+++ accessblobstorage/accessblobstorage/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Configuration;
+
+namespace accessblobstorage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = System.Configuration.ConfigurationManager.AppSettings.Get("StorageConnectionString");
+            CloudStorageAccount myStorageAccount = CloudStorageAccount.Parse(connectionString);
+
+            CloudBlobClient blobClient = myStorageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference("myblobs");
+            container.CreateIfNotExists();
+
+            ListAttributes(container);
+            SetMetadata(container);
+            ListMetadata(container);
+
+            
+            
+
+        }
+        public static void Uploadblob(CloudBlobContainer container)
+        {
+            using (var filestream = System.IO.File.OpenRead(@"C:\\Users\\Justin Macharia\\Pictures\\Camera Roll\\WIN_20180918_18_02_36_Pro.jpg"))
+            {
+                CloudBlockBlob blockblob = container.GetBlockBlobReference("images");
+                blockblob.UploadFromStream(filestream);
+            }
+        }
+
+        public static void ListAttributes(CloudBlobContainer container)
+        {
+            container.FetchAttributes();
+            Console.WriteLine("container name " + container.StorageUri.PrimaryUri.ToString());
+            Console.WriteLine("last modified " + container.Properties.LastModified.ToString());
+            Console.ReadLine();
+        }
+
+        public static void SetMetadata(CloudBlobContainer container)
+        {
+            container.Metadata.Clear();
+            container.Metadata.Add("Author", "Justin Macharia");
+            container.Metadata["authoredOn"] = "October 30, 2018";
+            container.SetMetadata();
+        }
+
+        public static void ListMetadata(CloudBlobContainer container)
+        {
+            container.FetchAttributes();
+            foreach (var item in container.Metadata)
+            {
+                Console.WriteLine("key " + item.Key);
+                Console.WriteLine("Value " + item.Value);
+                Console.ReadLine();
+            }
+        }
+    }
+}
